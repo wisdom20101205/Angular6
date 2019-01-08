1e248a7e48a6a985b99847fe61862cbdea498c31
Tiger <wisdom20101205@outlook.com>
2019-01-07 19:06:21 -0800

    intial project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { DateComponent } from './date/date.component';
+import { AddressCardComponent } from './address-card/address-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    DateComponent,
+    AddressCardComponent
   ],
   imports: [
     BrowserModule,
