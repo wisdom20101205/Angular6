616e7fdf3f6fa615772dece2722d108df26ea6ea
Tiger <wisdom20101205@outlook.com>
2019-01-08 05:08:22 -0800

    handling click event

--- src/app/address-card/address-card.component.ts
+++ src/app/address-card/address-card.component.ts
@@ -10,9 +10,11 @@ export class AddressCardComponent implements OnInit {
 
   user: any;
   // tslint:disable-next-line:no-input-rename
-  // @Input('name') userName: string;
+  @Input('name') userName: string;
   // tslint:disable-next-line:no-input-rename
   @Input('user') userObj: User;
+  // tslint:disable-next-line:no-inferrable-types
+  isCollapsed: boolean = true;
 
   constructor() {
     console.log('constructor');
@@ -37,6 +39,10 @@ export class AddressCardComponent implements OnInit {
       phone: this.userObj.phone
     };
 
+  }
+
+  toggleCollapse() {
+    this.isCollapsed = !this.isCollapsed;
   }
 
 }
